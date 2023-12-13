4aadd33deb1300f72e616211ca3470bcf62302ba
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2023-12-13 17:51:31 -0300

    Added logging, CLI args and extended the default template

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from mdit_py_plugins.front_matter import front_matter_plugin
 from jinja2 import Environment, FileSystemLoader
 import json
 from pathlib import Path
+import logging
+import argparse
 
 def render_page(text):
     md = MarkdownIt('commonmark').use(front_matter_plugin)
 
-    context = {}
+    context = {'template' : 'default.html'}
     tokens = md.parse(text)
-    if len(tokens) >= 1 and tokens[0].type == 'front_matter':
-        context = json.loads(tokens[0].content)
-
-    if 'template' not in context:
-        context['template'] = 'default.html'
+    if tokens and tokens[0].type == 'front_matter':
+        front_matter = json.loads(tokens[0].content)
+        context.update(front_matter)        
     context['body'] = md.render(text)
 
     env = Environment(loader=FileSystemLoader('templates'))
@@ -22,26 +22,38 @@ def render_page(text):
 
     return rendered
 
-def mirror_build(input_dir, output_dir):
+def generate_site(input_dir, output_dir):
     input_path = Path(input_dir)
     output_path = Path(output_dir)
 
     output_path.mkdir(parents=True, exist_ok=True)
 
-    for file in input_path.rglob('*md'):
-        relative = file.relative_to(input_path)
+    for md_file in input_path.rglob('*md'):
+        relative = md_file.relative_to(input_path)
         html_path = output_path / relative.with_suffix('.html')
         html_path.parent.mkdir(parents=True, exist_ok=True)
 
-        with open(file) as md_file:
-            md_content = md_file.read()
+        with md_file.open() as f:
+            md_content = f.read()
 
         html_content = render_page(md_content)
+        with html_path.open('w') as f:
+            f.write(html_content)
 
-        with open(html_path, 'w') as html_file:
-            html_file.write(html_content)
+        logging.info(f'Processed: {md_file} -> {html_path}')
 
-input_dir = 'content'
-output_dir = 'output'
 
-mirror_build(input_dir, output_dir)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate a website from markdown files.')
+    parser.add_argument('--input', '-i', default='content', help='Input directory with the markdown files.')
+    parser.add_argument('--output', '-o', default='output', help='Output directory to store generated HTML files.')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    input_dir = 'content'
+    output_dir = 'output'
+
+    logging.info(f'Starting build. Input directory: {input_dir}, Output directory {output_dir}')
+    generate_site(input_dir, output_dir)
+    logging.info('Mirror build completed.')
